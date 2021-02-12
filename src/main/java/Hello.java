28e3cc96dcfa2acdd6ceea4a5c8360e75ee2780d
esrabuyuktas <esrabuyuktas6@gmail.com>
2021-02-12 19:21:55 +0000

    I have update my Hello class

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Class");
         //more changes are coming
+
         //i am ready to commit this
 
     }
