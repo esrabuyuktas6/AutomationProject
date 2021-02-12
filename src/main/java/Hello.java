d703c9fbe33bf1a6217c4405ba81d8f81255fc97
esrabuyuktas <esrabuyuktas6@gmail.com>
2021-02-12 19:20:00 +0000

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Class");
+        //more changes are coming
+        //i am ready to commit this
 
     }
 
